63d3f6c1bb744dc56cf78b20fe297c9f74da94f9
Goataso <goat@viallance.com>
2025-02-27 22:30:22 +0400

    Add all package files

--- dist/cjs/utils/ai.cjs
+++ dist/cjs/utils/ai.cjs
@@ -38,7 +38,7 @@ class Ai {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}, response: ${await response.text()}`);
             }
-            const chatCompletion = await response.cjson();
+            const chatCompletion = await response.json();
             const responseMessage = chatCompletion.choices[0].message.content;
             const finalMessage = [];
             const limit = 2000;
