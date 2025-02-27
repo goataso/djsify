ef1e6afbda82af9f8c5f5c2c0a11efbed9eeaf40
Goataso <goat@viallance.com>
2025-02-27 22:15:43 +0400

    Add all package files

--- dist/esm/type/ButtonCommandFile.mjs
+++ dist/esm/type/ButtonCommandFile.mjs
@@ -2,7 +2,7 @@ class ButtonCommandFile {
     /**
      * @readonly
      * @param {object} data - The command data
-     * @param {(interaction: ButtonInteraction) => Promise<void> | void} execute - The execute function
+     * @param {Function} execute - The execute function
      */
     constructor(data, execute) {
         this.data = data;
