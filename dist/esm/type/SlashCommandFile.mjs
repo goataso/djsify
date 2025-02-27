2376d276120a6f2f0ec2f2e473027a5922d8a999
Goataso <goat@viallance.com>
2025-02-27 22:15:46 +0400

    Add all package files

--- dist/esm/type/SlashCommandFile.mjs
+++ dist/esm/type/SlashCommandFile.mjs
@@ -1,15 +1,13 @@
-import { CommandInteraction } from "discord.js";
-
 class SlashCommandFile {
     /**
      * @readonly
      * @param {object} data - The command data
-     * @param {(interaction: CommandInteraction) => Promise<void> | void} execute - The execute function
+     * @param {Function} execute - The execute function
      */
     constructor(data, execute) {
         this.data = data;
         this.execute = execute;
     }
-};
-
+}
+;
 export { SlashCommandFile };
