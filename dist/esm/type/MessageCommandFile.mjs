c0bd6a251790e9d9eaa8552d0fde8fc91acff82d
Goataso <goat@viallance.com>
2025-02-27 22:15:45 +0400

    Add all package files

--- dist/esm/type/MessageCommandFile.mjs
+++ dist/esm/type/MessageCommandFile.mjs
@@ -1,10 +1,8 @@
-import { Message } from "discord.js";
-
 class MessageCommandFile {
     /**
      * @readonly
      * @param {object} data - The command data
-     * @param {(interaction: Message) => Promise<void> | void} execute - The execute function
+     * @param {Function} execute - The execute function
      */
     constructor(data, execute) {
         this.data = data;
