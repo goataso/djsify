8172d8cdbdd2690bedd94cdd66facea5fbe0d485
Goataso <goat@viallance.com>
2025-03-07 16:44:33 +0400

    Add all package files

---
+++ src/type/Commands.ts
@@ -0,0 +1,9 @@
+import { ButtonInteraction, Client, CommandInteraction, Message, OmitPartialGroupDMChannel } from "discord.js";
+import djsClient from "../Client/setup.js";
+
+type TypedClient = Client & { djsClient?: djsClient };
+type SlashCommandInteraction = CommandInteraction & { djsClient?: djsClient, client?: TypedClient };
+type ButtoncommandInteraction = ButtonInteraction & { djsClient?: djsClient, client?: TypedClient };
+type MessageInteraction = OmitPartialGroupDMChannel<Message> & { djsClient?: djsClient, client?: TypedClient };
+
+export type { SlashCommandInteraction, ButtoncommandInteraction, MessageInteraction };
