07630bfe768cc3b31bd72c97c08196aca3e82f4b
Goataso <goat@viallance.com>
2025-03-07 16:44:17 +0400

    Add all package files

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -4,7 +4,7 @@ import { MessageCommandFile } from './type/MessageCommandFile.js';
 import { Ai } from './utils/ai.js';
 import { SlashCommand, OptionType } from './builders/SlashCommand.js';
 import { Colors } from './utils/Colors.js';
-import { djsClient } from './Client/setup.js';
+import djsClient from './Client/setup.js';
 export { SlashCommandFile, ButtonCommandFile, MessageCommandFile };
 export { SlashCommand, OptionType };
 export { Ai, Colors };
