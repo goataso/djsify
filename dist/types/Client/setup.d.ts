1d2964f8d4bb372d3257afc9ed007a3e7e21e44a
Goataso <goat@viallance.com>
2025-03-07 16:44:16 +0400

    Add all package files

--- dist/types/Client/setup.d.ts
+++ dist/types/Client/setup.d.ts
@@ -1,18 +1,20 @@
-import { MessageCommandFile } from "src/type/MessageCommandFile";
-import { SlashCommandFile } from "src/type/SlashCommandFile";
-import { ButtonCommandFile } from "src/type/ButtonCommandFile";
+import { MessageCommandFile } from "../type/MessageCommandFile.js";
+import { SlashCommandFile } from "../type/SlashCommandFile.js";
+import { ButtonCommandFile } from "../type/ButtonCommandFile.js";
 import { ActivityType, Client, Collection, GatewayIntentBits, Partials } from "discord.js";
-export declare class djsClient {
+import { ButtoncommandInteraction, MessageInteraction, SlashCommandInteraction } from '../type/Commands.js';
+declare class djsClient {
     client: Client & {
         messageCommands?: Collection<string, MessageCommandFile>;
         slashCommands?: Collection<string, SlashCommandFile>;
         buttons?: Collection<string, ButtonCommandFile>;
+        djsClient?: djsClient;
     };
     private buttonDirectoryName;
     private slashCommandDirectoryName;
     private messageCommandDirectoryName;
-    private token;
     allowedGuilds: Set<string | null>;
+    private token;
     addAllowedGuilds(guildId: string): Set<string | null>;
     getAllowedGuilds(): (string | null)[];
     setSlashCommandDir(dirname: string): Promise<void>;
@@ -21,7 +23,30 @@ export declare class djsClient {
     setStatus(status: 'online' | 'idle' | 'dnd' | 'invisible'): Promise<void>;
     setActivity(name: string, type: ActivityType): Promise<void>;
     reload(): Promise<void>;
-    constructor({ token, buttonOn, slashCommandsOn, messageCommandsOn, slashCommandDir, ButtonCommandDir, messageCommandDir, status, activityName, activityType, restTimeout, sharding, intents, partials, prefex, allowBots, allowDM, isPrivateBot, }: {
+    ready(): true | Promise<unknown>;
+    /**
+     * @param {Object} options - The configuration options for the client
+     * @param {string} options.token - The Discord bot token
+     * @param {boolean} [options.buttonOn=false] - Whether to enable button commands
+     * @param {boolean} [options.slashCommandsOn=false] - Whether to enable slash commands
+     * @param {boolean} [options.messageCommandsOn=false] - Whether to enable message commands
+     * @param {string} [options.slashCommandDir] - Directory for slash commands
+     * @param {string} [options.ButtonCommandDir] - Directory for button commands
+     * @param {string} [options.messageCommandDir] - Directory for message commands
+     * @param {('online'|'idle'|'dnd'|'invisible')} [options.status='online'] - Bot status
+     * @param {string} [options.activityName] - Activity name for bot status
+     * @param {string} [options.activityType='Playing'] - Activity type for bot status
+     * @param {number} [options.restTimeout=25000] - REST API timeout
+     * @param {(number|'auto'|readonly number[])} [options.sharding='auto'] - Sharding configuration
+     * @param {Array} [options.intents] - Gateway intents
+     * @param {Array} [options.partials] - Partial structures
+     * @param {string} [options.prefex] - deprecated Use 'prefix' instead
+     * @param {string} [options.prefix=''] - Command prefix
+     * @param {boolean} [options.allowBots=false] - Allow bot interactions
+     * @param {boolean} [options.allowDM=true] - Allow DM interactions
+     * @param {boolean} [options.isPrivateBot=false] - Private bot mode
+     */
+    constructor({ token, buttonOn, slashCommandsOn, messageCommandsOn, slashCommandDir, ButtonCommandDir, messageCommandDir, consoleConfig, status, activityName, activityType, restTimeout, sharding, intents, partials, prefex, prefix, allowBots, allowDM, isPrivateBot, allowedGuilds, preCommandHook }: {
         token: string;
         buttonOn?: boolean;
         slashCommandsOn?: boolean;
@@ -29,6 +54,14 @@ export declare class djsClient {
         slashCommandDir?: string;
         ButtonCommandDir?: string;
         messageCommandDir?: string;
+        consoleConfig?: {
+            error?: boolean;
+            warn?: boolean;
+            info?: boolean;
+            debug?: boolean;
+            trace?: boolean;
+            log?: boolean;
+        };
         status?: 'online' | 'idle' | 'dnd' | 'invisible';
         activityName?: string;
         activityType?: keyof typeof ActivityType;
@@ -37,8 +70,17 @@ export declare class djsClient {
         intents?: Exclude<keyof typeof GatewayIntentBits, GatewayIntentBits>[];
         partials?: (keyof typeof Partials)[];
         prefex?: string;
+        prefix?: string;
         allowBots?: boolean;
         allowDM?: boolean;
         isPrivateBot?: boolean;
+        allowedGuilds: string[] | string | null;
+        preCommandHook: {
+            ready?: () => void;
+            message?: (message: MessageInteraction, callBack: (...args: [MessageInteraction, ...any]) => Promise<void> | void) => Promise<void> | void;
+            slashCommand?: (command: SlashCommandInteraction, callBack: (...args: [SlashCommandInteraction, ...any]) => Promise<void> | void) => Promise<void> | void;
+            button?: (command: ButtoncommandInteraction, callBack: (...args: [ButtoncommandInteraction, ...any]) => Promise<void> | void) => Promise<void> | void;
+        };
     });
 }
+export default djsClient;
