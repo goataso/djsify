f0b8c13fe5eea872b51bc99250601e33d13f61ec
Goataso <goat@viallance.com>
2025-03-14 14:31:09 +0400

    Add all package files

---
+++ dist/esm/cli/buttonExample.mjs
@@ -0,0 +1,11 @@
+const GetButtonExmaple = (buttonCustomId) => `import { ButtonInteraction } from 'discord.js';
+
+export default {
+  data: {
+    customId: "${buttonCustomId}"
+  },
+  async execute(interaction: ButtonInteraction) {
+    // Your code here
+  }
+};`;
+export { GetButtonExmaple };
