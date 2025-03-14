4718a17a97da52e060232f47bef984f4d1230477
Goataso <goat@viallance.com>
2025-03-14 14:31:01 +0400

    Add all package files

---
+++ dist/cjs/cli/buttonExample.cjs
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GetButtonExmaple = void 0;
+const GetButtonExmaple = (buttonCustomId) => `import { ButtonInteraction } from 'discord.js';
+
+export default {
+  data: {
+    customId: "${buttonCustomId}"
+  },
+  async execute(interaction: ButtonInteraction) {
+    // Your code here
+  }
+};`;
+exports.GetButtonExmaple = GetButtonExmaple;
