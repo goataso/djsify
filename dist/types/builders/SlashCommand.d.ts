df915129dd4a90ffa042aaa1d13b88f85cbc5821
Goataso <goat@viallance.com>
2025-03-14 14:31:18 +0400

    Add all package files

--- dist/types/builders/SlashCommand.d.ts
+++ dist/types/builders/SlashCommand.d.ts
@@ -1,3 +1,63 @@
+import { ApplicationCommandOptionType, APIApplicationCommandOptionChoice, APIApplicationCommandBasicOption, RESTPostAPIApplicationCommandsJSONBody } from "discord.js";
+interface BaseOption {
+    name: string;
+    description: string;
+    required?: boolean;
+}
+interface StringOption extends BaseOption {
+    type: ApplicationCommandOptionType.String;
+    choices?: APIApplicationCommandOptionChoice<string>[];
+}
+interface IntegerOption extends BaseOption {
+    type: ApplicationCommandOptionType.Integer;
+    choices?: APIApplicationCommandOptionChoice<number>[];
+    min_value?: number;
+    max_value?: number;
+}
+interface NumberOption extends BaseOption {
+    type: ApplicationCommandOptionType.Number;
+    choices?: APIApplicationCommandOptionChoice<number>[];
+    min_value?: number;
+    max_value?: number;
+}
+interface BooleanOption extends BaseOption {
+    type: ApplicationCommandOptionType.Boolean;
+}
+interface UserOption extends BaseOption {
+    type: ApplicationCommandOptionType.User;
+}
+interface ChannelOption extends BaseOption {
+    type: ApplicationCommandOptionType.Channel;
+}
+interface RoleOption extends BaseOption {
+    type: ApplicationCommandOptionType.Role;
+}
+interface MentionableOption extends BaseOption {
+    type: ApplicationCommandOptionType.Mentionable;
+}
+interface AttachmentOption extends BaseOption {
+    type: ApplicationCommandOptionType.Attachment;
+}
+interface SubcommandOption extends BaseOption {
+    type: ApplicationCommandOptionType.Subcommand;
+    options?: APIApplicationCommandBasicOption[];
+}
+interface SubcommandGroupOption extends BaseOption {
+    type: ApplicationCommandOptionType.SubcommandGroup;
+    options?: APIApplicationCommandBasicOption[];
+}
+type Option = StringOption | IntegerOption | NumberOption | BooleanOption | UserOption | ChannelOption | RoleOption | MentionableOption | AttachmentOption | SubcommandOption | SubcommandGroupOption;
+export declare class SlashCommand {
+    private name;
+    private description;
+    private options;
+    private dm_permission?;
+    private default_member_permissions?;
+    setName(name: string): this;
+    setDescription(description: string): this;
+    addOption(option: Partial<Option>): this;
+    toJSON(): RESTPostAPIApplicationCommandsJSONBody;
+}
 export declare enum OptionType {
     STRING = 3,
     INTEGER = 4,
@@ -10,29 +70,5 @@ export declare enum OptionType {
     ATTACHMENT = 11,
     SUBCOMMAND = 1,
     SUBCOMMAND_GROUP = 2
-}
-interface ChoicesOptions {
-    name: string;
-    value: string;
-}
-interface Option {
-    type: OptionType | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
-    name: string;
-    description: string;
-    required?: boolean;
-    choices?: ChoicesOptions;
-}
-export declare class SlashCommand {
-    private name;
-    private description;
-    private options;
-    setName(name: string): this;
-    setDescription(description: string): this;
-    addOption(option: Option): this;
-    toJSON(): {
-        name: string;
-        description: string;
-        options: Option[];
-    };
 }
 export {};
